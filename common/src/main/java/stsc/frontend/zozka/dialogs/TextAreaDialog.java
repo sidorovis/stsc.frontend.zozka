70a2d1313ba7b6b74cec5933ac24deef4b74fa3c
Ivan Sidarau <isidarau@360t.com>
2015-10-16 09:53:51 +0200

    #4 added common module with TextAreaDialog for exceptions and etc

--- common/src/main/java/stsc/frontend/zozka/dialogs/TextAreaDialog.java
+++ common/src/main/java/stsc/frontend/zozka/dialogs/TextAreaDialog.java
@@ -1,34 +1,39 @@
 package stsc.frontend.zozka.dialogs;
 
-import javafx.scene.control.ButtonBar.ButtonData;
+import javafx.scene.control.Alert;
 import javafx.scene.control.ButtonType;
-import javafx.scene.control.Dialog;
 import javafx.scene.control.TextArea;
-import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.GridPane;
+import javafx.scene.layout.Priority;
 import javafx.stage.Stage;
-import javafx.util.Callback;
 
-public class TextAreaDialog extends Dialog<Void> {
+/**
+ * This is a dialog windows (GUI) for showing large texts with possibility to
+ * copy them (text area). Resize-able, not editable.
+ */
+public final class TextAreaDialog extends Alert {
 
 	private final TextArea textArea;
-	private final BorderPane borderPane = new BorderPane();
 
 	public TextAreaDialog(Stage owner, String title, String value) {
-		super(); // (owner, title);
+		super(AlertType.NONE);
 		setTitle(title);
+		setHeaderText(null);
+		setContentText(null);
 		this.textArea = new TextArea(value);
-		setWidth(600);
-		setHeight(600);
-		getDialogPane().setContent(borderPane);
-		borderPane.setCenter(textArea);
-		ButtonType buttonTypeOk = new ButtonType("Okay", ButtonData.OK_DONE);
-		getDialogPane().getButtonTypes().add(buttonTypeOk);
-		setResultConverter(new Callback<ButtonType, Void>() {
-			@Override
-			public Void call(ButtonType param) {
-				return null;
-			}
-		});
+		textArea.setEditable(false);
+		textArea.setWrapText(true);
+		textArea.setMaxWidth(Double.MAX_VALUE);
+		textArea.setMaxHeight(Double.MAX_VALUE);
+		GridPane.setVgrow(textArea, Priority.ALWAYS);
+		GridPane.setHgrow(textArea, Priority.ALWAYS);
+		GridPane expContent = new GridPane();
+		expContent.setMaxWidth(Double.MAX_VALUE);
+		expContent.add(textArea, 0, 0);
+		getDialogPane().setContent(expContent);
+		getButtonTypes().add(ButtonType.CLOSE);
+		setResizable(true);
+		getDialogPane().setPrefSize(600, 600);
 	}
 
 }
---
+++ common/src/test/java/stsc/frontend/zozka/dialogs/VisualTestTextAreaDialog.java
@@ -0,0 +1,23 @@
+package stsc.frontend.zozka.dialogs;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class VisualTestTextAreaDialog extends Application {
+
+	@Override
+	public void start(Stage parent) throws Exception {
+		final TextAreaDialog ad = new TextAreaDialog(parent, "Strategy: 14",
+				"hello world\nresult\n " + System.getProperty("java.version") + "\n"
+						+ "This is a long text. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et "
+						+ "dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, "
+						+ "no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy "
+						+ "eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et "
+						+ "ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.");
+		ad.showAndWait();
+	}
+
+	public static void main(String[] args) {
+		Application.launch(VisualTestTextAreaDialog.class, (java.lang.String[]) null);
+	}
+}
