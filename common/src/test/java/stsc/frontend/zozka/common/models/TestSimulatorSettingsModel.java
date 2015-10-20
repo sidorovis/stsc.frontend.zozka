790665e837b957878b067d4a31e978b067e4563c
Ivan Sidarau <isidarau@360t.com>
2015-10-20 15:07:39 +0200

    #4 added TestSimulatorSettingsModel

---
+++ common/src/test/java/stsc/frontend/zozka/common/models/TestSimulatorSettingsModel.java
@@ -0,0 +1,103 @@
+package stsc.frontend.zozka.common.models;
+
+import static org.junit.Assert.assertEquals;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.rules.TemporaryFolder;
+
+import javafx.collections.ObservableList;
+import stsc.common.algorithms.AlgorithmType;
+
+public class TestSimulatorSettingsModel {
+
+	@Rule
+	public TemporaryFolder testFolder = new TemporaryFolder();
+
+	@Test
+	public void testSaveLoad() throws FileNotFoundException, IOException, ClassNotFoundException {
+		final File f = testFolder.newFile("testFile");
+		{
+			final SimulatorSettingsModel model = new SimulatorSettingsModel();
+			final ExecutionDescription ed1 = new ExecutionDescription(AlgorithmType.STOCK_VALUE, "ename1", "in_algorithm");
+			ed1.addNumberAlgorithm(new NumberAlgorithmParameter("firstPN", ParameterType.INTEGER, "1", "2", "5"));
+			ed1.addNumberAlgorithm(new NumberAlgorithmParameter("secondPN", ParameterType.DOUBLE, "5.23", "3.12", "23.54"));
+			ed1.addTextAlgorithm(new TextAlgorithmParameter("thirdPN", ParameterType.SUB_EXECUTION, TextAlgorithmParameter.createDomenRepresentation("'asd','fds','greg','tyjt'")));
+			ed1.getTextAlgorithms()
+					.add(new TextAlgorithmParameter("fourthPN", ParameterType.STRING, TextAlgorithmParameter.createDomenRepresentation("'in','out','step','bbb','hello','world'")));
+			model.add(ed1);
+
+			final ExecutionDescription ed2 = new ExecutionDescription(AlgorithmType.EOD_VALUE, "eNewName1", "OneExample");
+			ed2.addNumberAlgorithm(new NumberAlgorithmParameter("TheSecondPN", ParameterType.DOUBLE, "2.54", "4.12", "26.12"));
+			ed2.addNumberAlgorithm(new NumberAlgorithmParameter("TheFirstPN", ParameterType.INTEGER, "6", "3", "55"));
+			ed2.addTextAlgorithm(new TextAlgorithmParameter("TheFourthPN", ParameterType.STRING,
+					TextAlgorithmParameter.createDomenRepresentation("'I','hate','stupid','people','who','are_not_writing_tests'")));
+			ed2.addTextAlgorithm(
+					new TextAlgorithmParameter("TheThirdPN", ParameterType.SUB_EXECUTION, TextAlgorithmParameter.createDomenRepresentation("'And','happinnes','always','gone'")));
+			model.add(ed2);
+			try (OutputStream os = new FileOutputStream(f)) {
+				model.saveToFile(os);
+			}
+		}
+		{
+			final SimulatorSettingsModel model = new SimulatorSettingsModel();
+			try (InputStream is = new FileInputStream(f)) {
+				model.loadFromFile(is);
+			}
+			assertEquals(2, model.size());
+
+			final ObservableList<NumberAlgorithmParameter> nap0 = model.getModel().get(0).getNumberAlgorithms();
+			final ObservableList<TextAlgorithmParameter> tap0 = model.getModel().get(0).getTextAlgorithms();
+			final ObservableList<NumberAlgorithmParameter> nap1 = model.getModel().get(1).getNumberAlgorithms();
+			final ObservableList<TextAlgorithmParameter> tap1 = model.getModel().get(1).getTextAlgorithms();
+
+			assertEquals(2, nap0.size());
+			assertEquals(2, tap0.size());
+			assertEquals(2, nap1.size());
+			assertEquals(2, tap1.size());
+
+			assertEquals(ParameterType.INTEGER, nap0.get(0).getType());
+			assertEquals(ParameterType.DOUBLE, nap0.get(1).getType());
+
+			assertEquals(ParameterType.INTEGER, nap1.get(1).getType());
+			assertEquals(ParameterType.DOUBLE, nap1.get(0).getType());
+
+			assertEquals(ParameterType.SUB_EXECUTION, tap0.get(0).getType());
+			assertEquals(ParameterType.STRING, tap0.get(1).getType());
+
+			assertEquals(ParameterType.SUB_EXECUTION, tap1.get(1).getType());
+			assertEquals(ParameterType.STRING, tap1.get(0).getType());
+
+			assertEquals("1", nap0.get(0).getFrom());
+			assertEquals("2", nap0.get(0).getStep());
+			assertEquals("5", nap0.get(0).getTo());
+
+			assertEquals("5.23", nap0.get(1).getFrom());
+			assertEquals("3.12", nap0.get(1).getStep());
+			assertEquals("23.54", nap0.get(1).getTo());
+
+			assertEquals("6", nap1.get(1).getFrom());
+			assertEquals("3", nap1.get(1).getStep());
+			assertEquals("55", nap1.get(1).getTo());
+
+			assertEquals("2.54", nap1.get(0).getFrom());
+			assertEquals("4.12", nap1.get(0).getStep());
+			assertEquals("26.12", nap1.get(0).getTo());
+
+			assertEquals(4, TextAlgorithmParameter.createDomenRepresentation(tap0.get(0).domenProperty().getValue()).size());
+			assertEquals(6, TextAlgorithmParameter.createDomenRepresentation(tap0.get(1).domenProperty().getValue()).size());
+
+			assertEquals(6, TextAlgorithmParameter.createDomenRepresentation(tap1.get(0).domenProperty().getValue()).size());
+			assertEquals(4, TextAlgorithmParameter.createDomenRepresentation(tap1.get(1).domenProperty().getValue()).size());
+		}
+		assertEquals(true, f.delete());
+	}
+}
